cac59da417d308b394b25f7e0938ebf369c6375e
M-Hasnain-R <hasnainrazavhr000@gmail.com>
2026-02-18 14:48:26 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -16,7 +16,7 @@ float getFloatValue() {
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
-    char var_char = 'A';
+    
 }
 
 int convertFloatToInt(float value) {
